509991bc311ec06c0575644e6c47bbd9498b1fc7
Andrea Ciceri <andrea.ciceri@autistici.org>
2022-07-06 15:49:13 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
     packages.x86_64-linux.default = ./.;
     hydraJobs = {
       build = pkgs.bash.overrideAttrs(old: {
-        name = "bash-sdgfrtgrtghhjghefg"; # To trigger Hydra
+        name = "bash-sdgfrtgrtghhfghfghgjghefg"; # To trigger Hydra
       });
     };
   };
